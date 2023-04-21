18277ebe0ae5a10b617428b03a1dee3caf48731f
Ceo-Sammarco <noah.kd@gmail.com>
2023-04-21 20:44:55 -0300

    Ceo - sex 21 abr 2023 20:44:55 -03

---
+++ client/src/views/about.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ceo from "../utils/ceo";
+import "./app.css";
+
+
+export default About = () => {
+    const [ open, setOpen ] = useState( false );
+
+    return( <>
+        <t1>About</t1>
+    </> );
+}
