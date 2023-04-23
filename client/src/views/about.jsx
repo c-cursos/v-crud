6a5866c4e20b8a4fd7b9ff67f7af720bc6a67a0a
Ceo <noah.kd@gmail.com>
2023-04-23 10:52:18 -0300

    update client/src/views/about.jsx

--- client/src/views/about.jsx
+++ client/src/views/about.jsx
@@ -4,9 +4,32 @@ import React from "react";
 
 
 export default class About extends React.Component {
-  render() {
-    return <h1>About</h1>;
-  }
+    render() { return( <>
+        <style>
+            galerry {
+                display: grid;
+                grid-auto-flow: row;
+                gap: 2rem;
+                border-radius: 13px;
+                background: #f5f5f5;
+                box-shadow: #fff 0 0 0 13px;
+                padding: 2rem;
+            }
+        </style>
+        <galerry>
+            <t1>About</t1>
+            <hr />
+            <label>
+                <input type="text" placeholder="preço" />
+            </label>
+            <label>
+                <input type="text" placeholder="preço antigo" />
+            </label>
+            <label>
+                <input type="text" placeholder="desconto" />
+            </label>
+        </galerry>
+    </> ); }
 }
 
 
