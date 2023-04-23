775f759fa1cdb21d10a0a59d5322d50734bac1ea
Ceo <noah.kd@gmail.com>
2023-04-23 10:54:08 -0300

    update client/src/views/about.jsx

--- client/src/views/about.jsx
+++ client/src/views/about.jsx
@@ -1,21 +1,11 @@
 
 
 import React from "react";
+import "./about.css";
 
 
 export default class About extends React.Component {
     render() { return( <>
-        <style>
-            galerry {
-                display: grid;
-                grid-auto-flow: row;
-                gap: 2rem;
-                border-radius: 13px;
-                background: #f5f5f5;
-                box-shadow: #fff 0 0 0 13px;
-                padding: 2rem;
-            }
-        </style>
         <galerry>
             <t1>About</t1>
             <hr />
